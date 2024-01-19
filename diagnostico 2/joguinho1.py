1ad984b4483b794e7c82df910d8317893c1d6730
arthurgobira <arthurgobira5@gmail.com>
2024-01-19 09:51:02 -0300

    1

---
+++ diagnostico 2/joguinho1.py
@@ -0,0 +1,20 @@
+import random
+
+aleatorio = random.randint(1, 100)
+tentativas = 0
+palpite = 0
+print("Jogo de advinhação")
+
+while tentativas <=8 or palpite == aleatorio:
+    palpite = int(input("Qual o seu palpite? "))
+    tentativas += 1
+    if palpite > aleatorio:
+        print("Menos...")
+    if palpite < aleatorio:
+        print("Mais...")
+    if palpite == aleatorio:
+        print("Acertou em {} tentativas".format(tentativas))
+        break
+
+if palpite != aleatorio:
+    print("Perdeu, o número era {}".format(aleatorio))
---
+++ diagnostico 2/joguinho2.py
@@ -0,0 +1,31 @@
+import random 
+
+print ("Jogo de advinhação")
+print ("Vamos jogar!! Pense em um número entre 1 e 100")
+acertei = False
+tentativas = 1
+maior = 100
+menor = 1
+aleatorio = random.randint(1, 100)
+lista = []
+
+while acertei == False:
+    lista.append(aleatorio)
+    print ("Meu palpite é: " + str(aleatorio))
+    print ("O numero que você pensou é maior ou menor que " + str(aleatorio) + "?")
+    resposta = int (input("Digite 1 para maior, 2 para menor, 3 para acertou: "))
+    if resposta == 1:
+        menor = aleatorio
+    if resposta == 2:
+        maior = aleatorio
+    if resposta == 3:
+        acertei = True
+    aleatorio = (menor + maior) // 2
+    print ("eu tentei " + str(tentativas) + " vezes")
+    tentativas = tentativas + 1
+if acertei == True:
+    print ("Parabens para mim, eu acertei! Em " + str(tentativas-1) + " tentativas")
+if tentativas > 8:
+    x = int(input("Que pena, eu perdi! Qual era o numero?"))
+    print ("Você trapacecou ")
+    
