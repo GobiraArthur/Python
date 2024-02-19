dbb5ec50b7e12f1b0cf50c28f8bb6713b0f82d7f
Arthur Gobira <arthurgobira5@gmail.com>
2024-02-19 11:06:48 -0300

    1

---
+++ TrilhaS8/AlunosDeUmaEscola.py
@@ -0,0 +1,40 @@
+import numpy as np
+from faker import Faker
+import pandas as pd
+
+faker = Faker('pt_BR')
+x = 1000
+sexo = [faker.boolean() for _ in range(x)]
+nome=[]
+for _ in range(x):
+    if sexo[_] == True:
+        nome.append(faker.name_male())
+    else:
+        nome.append(faker.name_female())
+cpf = [faker.cpf() for _ in range(x)]
+
+idade = np.random.randint(18, 29, x)
+email = [faker.email() for _ in range(x)]
+notaEnem = np.random.randint(640, 801, x)
+abandonou = [faker.boolean(chance_of_getting_true=30) for _ in range(x)]
+cra2 = np.full(x, np.nan)
+cra4 = np.full(x, np.nan)
+cra6 = np.full(x, np.nan)
+semestreAbandonou = [None]*x
+for i in range(x):
+    semestreAbandon = np.random.randint(1, 7)
+    if abandonou[i] == False:
+        cra2[i] = round(np.random.uniform(5, 10),2)
+        cra4[i] = round(np.random.uniform(5, 10),2)
+        cra6[i] = round(np.random.uniform(5, 10),2)
+    else:
+        semestreAbandonou[i] = semestreAbandon
+        if semestreAbandon > 2 and semestreAbandon <= 4:
+            cra2[i] = round(np.random.uniform(5, 10),2)
+        if semestreAbandon > 4 and semestreAbandon <= 6:
+            cra2[i] = round(np.random.uniform(5, 10),2)
+            cra4[i] = round(np.random.uniform(5, 10),2)
+    
+dados = {'Nome': nome, 'CPF': cpf, 'Idade': idade, 'Email': email, 'Nota Enem': notaEnem, 'CRA2': cra2, 'CRA4': cra4, 'CRA6': cra6, 'Abandonou': abandonou, 'Semestre Abandonou': semestreAbandonou}
+df = pd.DataFrame(dados)
+df.to_csv('Alunos.csv')
